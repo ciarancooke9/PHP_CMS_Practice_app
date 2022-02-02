461219562ed5d24f36be39061fd283c649063884
cookec9 <ciaran.cooke9@mail.dcu.ie>
2022-02-02 10:02:06 +0000

    admin posts page table working

--- cms/admin/includes/navigation.php
+++ cms/admin/includes/navigation.php
@@ -36,7 +36,7 @@
                 <a href="javascript:;" data-toggle="collapse" data-target="#posts"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts" class="collapse">
                     <li>
-                        <a href="#">View all posts</a>
+                        <a href="post.php">View all posts</a>
                     </li>
                     <li>
                         <a href="#">Add posts</a>
